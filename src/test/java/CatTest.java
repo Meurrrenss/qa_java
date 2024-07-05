73e7b4cb5fc955068bff2530a251d5842130efe0
Dmitriy Lazutin <dlazutin@sberhealth.ru>
2024-07-05 18:49:13 +0300

    tests to git

--- src/test/java/CatTest.java
+++ src/test/java/CatTest.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class CatTest {
+import com.example.Cat;
+import com.example.Feline;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.List;
+
+import static junit.framework.TestCase.assertEquals;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CatTest {
+
+    @Mock
+    Feline feline;
+
+    @Test
+    public  void testGetSound() {
+        Cat cat = new Cat(feline);
+        cat.getSound();
+        assertEquals("Мяу", cat.getSound());
+    }
+
+    @Test
+    public void testGetFood() throws Exception {
+        Cat cat = new Cat(feline);
+        Mockito.when(feline.eatMeat()).thenReturn(List.of("Животные", "Птицы", "Рыба"));
+        assertEquals(cat.getFood(), List.of("Животные", "Птицы", "Рыба"));
+    }
 }
